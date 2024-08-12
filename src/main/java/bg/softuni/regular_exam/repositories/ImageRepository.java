2b1710e81be47ad3eee161a3308576534172e4e2
OneDuckyBoy <stilianmatev@gmail.com>
2024-08-12 14:25:13 +0300

    fixed image repository

--- src/main/java/bg/softuni/regular_exam/repositories/ImageRepository.java
+++ src/main/java/bg/softuni/regular_exam/repositories/ImageRepository.java
@@ -2,9 +2,11 @@ package bg.softuni.regular_exam.repositories;
 
 import bg.softuni.regular_exam.models.entity.ImagesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ImageRepository extends JpaRepository<ImagesEntity,Long> {
     ImagesEntity findById(long id);
     Optional<ImagesEntity> findFirstByImageLocationEndingWith(String name);
